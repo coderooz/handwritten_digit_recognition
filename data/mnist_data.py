e4c9cbc4ad3c141b197df00406daa28ed27f18ca
Ranit Saha <162082159+coderooz@users.noreply.github.com>
2024-07-24 20:22:23 +0530

    Create mnist_data.py

---
+++ data/mnist_data.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+from tensorflow.keras.datasets import mnist
+
+def load_data():
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    x_train = x_train / 255.0
+    x_test = x_test / 255.0
+    x_train = x_train.reshape(-1, 28, 28, 1)
+    x_test = x_test.reshape(-1, 28, 28, 1)
+    return (x_train, y_train), (x_test, y_test)
